83a7c494b3c97630fa5071e4ca893b134c37ee44
Ivanhdz04 <ivanrdguez04@gmail.com>
2025-02-05 21:21:27 -0600

    haciendo un ejemplo de ruta

---
+++ src/app/page-prueba/page.tsx
@@ -0,0 +1,7 @@
+export default function PagePrueba() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold mb-4">Esta pagina es la otra pagina que </h1>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Hola mundo</Button>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">Página Principal</h1>
+      <Link href="/page">
+        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg">
+          Ir a Página de Prueba
+        </button>
+      </Link>
+
+      <Link href="/otra-prueba">
+        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg">
+          Ir a Página de Prueba
+        </button>
+      </Link>
     </div>
   );
 }
